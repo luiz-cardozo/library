052e10bf7df4e1c95af8a91c246dbc3949073988
Luiz Cardozo <luizfcardozo@gmail.com>
2025-01-17 19:40:32 -0300

    creates base layout for auth pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Sign in Form</div>;
+};
+export default Page;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Sign up Form</div>;
+};
+export default Page;
